409f74c86e2576869682ffeae79978cc60c5536f
我只想看看蓝天 <1207032539@qq.com>
2024-05-25 01:55:08 +0800

    增加翻译

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import io
 import os
 import typing
 
+import requests
 import torch
 from PIL import Image
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI, UploadFile, File, Query
 from pydantic import BaseModel
 from transformers import BlipProcessor, BlipForConditionalGeneration
 
@@ -24,7 +25,10 @@ class CommonResponse(BaseModel):
 
 
 @app.post("/api/predict")
-async def get_image_caption(file: UploadFile = File(...)):
+async def get_image_caption(
+        file: UploadFile = File(...),
+        target_lang: str = Query(None)
+):
     # 读取图像文件
     image_bytes = await file.read()
     image = Image.open(io.BytesIO(image_bytes))
@@ -36,7 +40,31 @@ async def get_image_caption(file: UploadFile = File(...)):
     # 生成描述
     caption = processor.decode(output[0], skip_special_tokens=True)
 
+    # 支持的语言列表
+    supported_langs = [
+        "af", "sq", "am", "ar", "hy", "az", "eu", "be", "bn", "bs", "bg", "ca", "ceb", "ny",
+        "zh", "zh-TW", "co", "hr", "cs", "da", "nl", "en", "eo", "et", "tl", "fi", "fr", "fy",
+        "gl", "ka", "de", "el", "gu", "ht", "ha", "haw", "he", "hi", "hmn", "hu", "is", "ig",
+        "id", "ga", "it", "ja", "jw", "kn", "kk", "km", "rw", "ko", "ku", "ky", "lo", "la",
+        "lv", "lt", "lb", "mk", "mg", "ms", "ml", "mt", "mi", "mr", "mn", "my", "ne", "no",
+        "or", "ps", "fa", "pl", "pt", "pa", "ro", "ru", "sm", "gd", "sr", "st", "sn", "sd",
+        "si", "sk", "sl", "so", "es", "su", "sw", "sv", "tg", "ta", "tt", "te", "th", "tr",
+        "tk", "uk", "ur", "ug", "uz", "vi", "cy", "xh", "yi", "yo", "zu"
+    ]
+    if target_lang and target_lang in supported_langs:
+        # 翻译成特定语言
+        caption = translate(caption, target_lang)
+
     return CommonResponse(code=200, data={'result': caption, })
+
+
+def translate(text: str, target_lang: str) -> str:
+    with requests.get('https://lingva.thedaviddelta.com/api/v1/auto/' + target_lang + '/' + text) as response:
+        text = ''
+        if response.status_code == 200:
+            data = response.json()
+            text = data['translation']
+        return text
 
 
 if __name__ == "__main__":
